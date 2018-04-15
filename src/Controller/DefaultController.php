6435c31a71f6b988191f8ae2dbc52c3159d46de4
Deimantas Abelskis <ddeimantass@gmail.com>
2018-04-15 11:09:33 +0300

    seo update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,18 +13,6 @@ class DefaultController extends Controller {
      */
     public function list() {
 
-        return $this->render('student/index.html.twig', array("students" => $this->getStudentData()));
-    }
-    /**
-     * @Route("/student/{id}", name="student")
-     */
-    public function student($id) {
-
-        return $this->render('student/show.html.twig', array("student" => $this->getStudentData($id)));
-    }
-
-    private function getStudentData($id = null){
-
         $data =  json_decode(file_get_contents(__DIR__.'/../../public/data.json'), true);
         $students = array();
         foreach ($data as $team => $members){
@@ -33,6 +21,19 @@ class DefaultController extends Controller {
             }
         }
 
-        return isset($id) && array_key_exists($id, $students) ? $students[$id] : $students;
+        return $this->render('student/index.html.twig', array("students" => $students));
+    }
+    /**
+     * @Route("/student", name="student")
+     */
+    public function student(Request $request ) {
+
+        return $this->render('student/show.html.twig', array(
+                "academy" => $request->get("utm_source"),
+                "mentor" => $request->get("utm_campaign"),
+                "name" => $request->get("utm_term"),
+                "team" => $request->get("utm_content"),
+                )
+        );
     }
 }
